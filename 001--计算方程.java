3ca19d750c323d1734ec0b8120104eefaa3c1263
DSTJZ <155414834+DSTJZ@users.noreply.github.com>
2024-03-01 19:05:13 +0800

    Update 001--计算方程.java

--- 001--计算方程.java
+++ 001--计算方程.java
@@ -1,11 +1,12 @@
 //蓝桥小白入门赛
 //https://www.lanqiao.cn/problems/16982/learning/?contest_id=171
+// 数论、二分
 
 import java.util.Scanner;
 
-public class Main
-{	public static void main(String[] args)
-	{	Scanner scanner = new Scanner(System.in);
+公共 class Main
+{	公共 static void main(String[] args)
+	{	Scanner scanner = 新建 Scanner(System.in);
 		int t = scanner.nextInt();
 		while(t-- > 0)
 		{	int k = scanner.nextInt();
@@ -22,10 +23,10 @@ public class Main
 			System.out.println(left);
 		}
 	}
-	public static boolean expression(int x, int k, int m)//判断当前的x是否满足条件
+	公共 static boolean expression(int x, int k, int m)//判断当前的x是否满足条件
 	{	return Math.sqrt(x) + log(k, x) - m > 0;
 	}
-	public static int log(int k, int x)
+	公共 static int log(int k, int x)
 	{	return (int)(Math.log(x) / Math.log(k));
 	}
 }
